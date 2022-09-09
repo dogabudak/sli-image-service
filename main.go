5e2a881ab5dfa97284ab9054e19eb7a12ee1cc41
Doga Budak <dogabudak@gmail.com>
2022-09-09 22:38:30 +0200

    returns the image for the service

--- main.go
+++ main.go
@@ -1,64 +1,23 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"fmt"
+	"io/ioutil"
 	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Route("/images", func(r chi.Router) {
-		r.Route("/{imageID}", func(r chi.Router) {
-			r.Use(ImageCtx)
-			r.Get("/", getImage)
-		})
-	})
-	http.ListenAndServe(":6057", r)
+	handler := http.HandlerFunc(handleRequest)
+	http.Handle("/photo", handler)
+	http.ListenAndServe(":8080", nil)
 }
 
-func ImageCtx(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		imageID := chi.URLParam(r, "imageID")
-		image, err := dbGetImage(imageID)
-		if err != nil {
-			http.Error(w, http.StatusText(404), 404)
-			return
-		}
-		ctx := context.WithValue(r.Context(), "image", image)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
-
-func getImage(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	image, ok := ctx.Value("image").(*Image)
-	if !ok {
-		http.Error(w, http.StatusText(422), 422)
-		return
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	fileBytes, err := ioutil.ReadFile("./images/2.jpg")
+	if err != nil {
+		panic(err)
 	}
-	w.Write([]byte(fmt.Sprintf("title:%s", image.Name)))
-}
-
-type Image struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-}
-
-var images = []*Image{
-	{ID: "1", Name: "Example.jpeg"},
-}
-
-func dbGetImage(id string) (*Image, error) {
-	for _, a := range images {
-		if a.ID == id {
-			return a, nil
-		}
-	}
-	return nil, errors.New("article not found.")
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(fileBytes)
+	return
 }
